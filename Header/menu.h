6521be33dad0e88ab581ee253ea9be21da40d967
nabilsaragih <nabilsaragih33@gmail.com>
2023-04-12 13:39:21 +0800

    Fixed menu

---
+++ Header/menu.h
@@ -0,0 +1,33 @@
+#ifndef MENU.H
+#define MENU.H
+
+#include <iostream>
+#include <conio.h>
+#include "interli.h"
+#include "reglog.h"
+
+void Menu()
+{
+    system("cls");
+    string deskripsi_menu[3] = {"Login", "Register", "Exit"};
+    string header_menu = "MENU UTAMA";
+    create_menus(banyak_opsi, deskripsi_menu, header_menu);
+
+    if (current_selection == 0)
+    {
+        Login();
+    }
+    else if (current_selection == 1)
+    {
+        Register();
+    }
+    else
+    {
+        exit(0);
+    }
+
+    getch();
+    Menu();
+}
+
+#endif
--- Header/reglog.h
+++ Header/reglog.h
@@ -169,28 +169,4 @@ void Login()
     }
 }
 
-void Menu()
-{
-    system("cls");
-    string deskripsi_menu[3] = {"Login", "Register", "Exit"};
-    string header_menu = "MENU UTAMA";
-    create_menus(banyak_opsi, deskripsi_menu, header_menu);
-
-    if (current_selection == 0)
-    {
-        Login();
-    }
-    else if (current_selection == 1)
-    {
-        Register();
-    }
-    else
-    {
-        exit(0);
-    }
-
-    getch();
-    Menu();
-}
-
 #endif
